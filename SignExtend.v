7cc52f12f6f411f1034d7caed3c366ab4166b22f
RobertMS6 <RobertMS6@gmail.com>
2014-04-01 18:27:39 -0400

    Added Sign Extend Module

---
+++ SignExtend.v
@@ -0,0 +1,8 @@
+module signExtend (extend, extended);
+    input [15:0] extend;
+    output reg [31:0] extended;
+
+    always @(extend) begin
+        extended[31:0] = {{31{extend[15]}}, extend[15:0]};
+    end
+endmodule
