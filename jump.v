370d1ac8364b6ed6adec6250f0f5763d42b8adba
RobertMS6 <RobertMS6@gmail.com>
2014-04-03 12:44:01 -0400

    Fixed jump/PC/branch logic, now compiles

--- jump.v
+++ jump.v
@@ -1,22 +1,25 @@
 module jump(instruction, pc, zero, branch_sel, jump_sel);
     input [31:0] instruction, pc;
-    input [0:0] zero, branch_sel, jump_sel, branch_ctl;
+    input [0:0] zero, branch_sel, jump_sel;
+    reg [0:0] branch_ctl;
     reg [15:0] instrTemp;
-    reg [31:0] newPC;
-    wire [31:0] pcTemp, bInstr, jInstr, addrTemp;
+    reg [31:0] pcTemp, bInstrTemp, jInstrTemp;
+    output [31:0] newPC;
+    output [31:0] bInstr, jInstr, addrTemp;
 
-    always @(pc) begin
+    signExtend bTemp(instrTemp, bInstr);
+    twoToOneMux_32 branchMux(pcTemp, bInstrTemp, branch_ctl, addrTemp);
+    twoToOneMux_32 jumpMux(addrTemp, jInstrTemp, jump_sel, newPC);
+
+    always @* begin
         pcTemp = pc + 4;
         instrTemp = instruction[15:0];
-        signExtend bTemp(instrTemp, bInstr);
-        bInstr = bInstr * 4;
-        bInstr = bInstr + pcTemp;
+        // signExtend bTemp(instrTemp, bInstr);
+        bInstrTemp = (bInstr << 2) + pcTemp;
         branch_ctl = zero & branch_sel;
-        twoToOneMux_32 branchMux(pcTemp, bInstr, branch_ctl, addrTemp);
-
-        jInstr = instruction[25:0] * 4;
-        jInstr = {{31{jInstr[0]}}, jInstr[27:0]};
-        twoToOneMux_32 jumpMux(addrTemp, jInstr, jump_sel, newPC);
+        // twoToOneMux_32 branchMux(pcTemp, bInstrTemp, branch_ctl, addrTemp);
+        jInstrTemp = {{31{jInstr[0]}}, instruction[25:0] << 2};
+        // twoToOneMux_32 jumpMux(addrTemp, jInstrTemp, jump_sel, newPC);
     end
 
 endmodule
